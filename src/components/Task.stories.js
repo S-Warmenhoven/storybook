b07d67eaca7ed4e10c08d7ca7a3f420f872a0d94
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-02-15 18:06:53 -0800

    created add-on, Knobs

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import Task from './Task';
 
 export default {
   component: Task,
   title: 'Task',
+  decorators: [withKnobs],
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,
 };
@@ -30,8 +32,12 @@ export const actionsData = {
   onArchiveTask: action('onArchiveTask'),
 };
 
+// export const Default = () => {
+//   return <Task task={{ ...taskData }} {...actionsData} />;
+// };
+
 export const Default = () => {
-  return <Task task={{ ...taskData }} {...actionsData} />;
+    return <Task task={object('task', { ...taskData })} {...actionsData} />;
 };
 
 // action() allows us to create a callback that appears 
@@ -52,4 +58,10 @@ export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {
 
 export const Archived = () => (
   <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+);
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = () => (
+  <Task task={{ ...taskData, title: longTitleString }} {...actionsData} />
 );
