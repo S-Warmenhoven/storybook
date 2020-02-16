4f83ca85616eddefaa6f77638e4b8f3533390b9a
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-02-15 20:09:42 -0800

    create own addon, assets tab

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -4,12 +4,20 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, object } from '@storybook/addon-knobs/react';
 
+
 import Task from './Task';
 
 export default {
   component: Task,
   title: 'Task',
   decorators: [withKnobs],
+  parameters: {
+    assets: [
+      "path/to/your/asset.png",
+      "path/to/another/asset.png",
+      "path/to/yet/another/asset.png"
+    ]
+  },
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,
 };
